8e887c113ed7c83c7c37846d70435dd73362c4fc
jacob <ghday.jacob@gmail.com>
2024-02-01 11:05:11 +0100

    klar med users

--- scripts/users.js
+++ scripts/users.js
@@ -1,5 +1,25 @@
 /* Här skapar ni en array bestående av minst 3 st användarobjekt */
 
 let users = [
-    
+    {
+        username: 'sandra',
+        password: 'sandra123',
+        email: 'sandra@gmail.com'
+    },
+    {
+        username: 'jacob',
+        password: 'jacob123',
+        email: 'jacob@gmail.com'
+    },
+    {
+        username: 'fredrick',
+        password: 'fredrick123',
+        email: 'fredrick@gmail.com'
+    },
+    {
+        username: 'najib',
+        password: 'najib123',
+        email: 'najib@gmail.com'
+    }
+
 ];
